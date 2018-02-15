34d4acab4d4b44bec62e3fe8f398dbf7cc550216
Liam McClelland <mcclelland-109773@3aaa.ac.uk>
2018-02-15 18:21:42 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,3 +5,15 @@ def turn_count(board)
       count += 1
     end
   end
+
+def current_player(board)
+  number = turn_count(board)
+  if number.even?
+    return "X"
+  elsif number.odd?
+    return "O"
+  end
+end
+
+b = [" "," "," ","X"," "," "," ","O","X"]
+ puts turn_count(b)
